aaa2859919776c61653cd6aea89cddc0293c5ab8
gautamsood15 <gauty22@gmail.com>
2020-02-24 04:42:53 +0530

    adding helicopter image

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import pygame 
 
+black = (0,0,0)
+white = (255,255,255)
+
 pygame.init()
-
 surface = pygame.display.set_mode((800,400))
-
 pygame.display.set_caption('Helicopter')
-
 clock = pygame.time.Clock()
+
+# Function area
+
+def helicopter(x,y,image):
+    surface.blit(image, (x,y))
+
+img = pygame.image.load('Helicopter.png')
+x = 150
+y = 200
+
+
+#game loop
+
 
 game_over = False
 
 while not game_over:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+
+    surface.fill(black)
+    helicopter(x,y,img)
 
     pygame.display.update()
 
