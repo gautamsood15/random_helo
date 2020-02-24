eeaf64730971f275663d476e4a237378569258fe
gautamsood15 <gauty22@gmail.com>
2020-02-24 06:05:44 +0530

    Adding the gameove() fxna nd paramenets

--- main.py
+++ main.py
@@ -16,9 +16,14 @@ y = 200
 
 y_move = 0
 
+
+def gameOver():
+    pygame.quit()
+    quit()    
+
+
 def helicopter(x,y,image):
     surface.blit(image, (x,y))
-
 
 
 #game loop
@@ -48,9 +53,9 @@ while not game_over:
 
     surface.fill(black)
     helicopter(x,y,img)
-
-
-
+    
+    if y > 360 or y < 0:
+        gameOver()
 
     pygame.display.update()
     clock.tick(60)   #means this  game will be 60 frames per sec
